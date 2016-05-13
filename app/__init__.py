5edd95bd31ee91dc42034cbe038345c6eba57e86
yvonnendutaw <yvonita7@gmail.com>
2016-05-13 08:53:01 +0300

    blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask import Blueprint
 
 app = Flask(__name__)
+auth = Blueprint('auth', __name__)
+
+def create_app(config_name)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='')
+
+    return app
+
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
+from . import auth
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('login')
+def login():
+    return render_template('login.html')
+
