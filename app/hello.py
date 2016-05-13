8a815e1bf063c39951ae7b5133c0026b0df7dc18
yvonnendutaw <yvonita7@gmail.com>
2016-05-13 14:10:12 +0300

    personalized hello.py

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1>'
+
+#personalized
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-app.run(debug=True)
+    app.run(debug=True)
