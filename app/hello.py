6a17a5e1b16d3e627c521421f32dda127d7d5756
yvonnendutaw <yvonita7@gmail.com>
2016-05-14 22:03:46 +0300

    class User

--- app/hello.py
+++ app/hello.py
@@ -1,63 +1,37 @@
-from flask import Flask, make_response, redirect, abort, render_template
+#!usr/bin/python
+
+from flask import Flask
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-#
-#@app.route('/index')
-#def index():
-#    return render_template('index.html')
-#
-#@app.route('/user/<name>')
-#def user(name):
-#    return render_template('user.html',
-#                           user=user)
 
 class NameForm(Form):
-    user = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+from flask import Flask, render_template
 
-#@app.route('/index')
-#def index():
-#     user = {'nickname':'jane'}
-#     return render_template('index.html',
-#                           user=user)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
-    user = None
+     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.user.data
-        form.user.data = ''
-        return render_template('index.html',
-                               form=form,
-                               user=user)
+        name = form.name.data
+        form.name.data = ''
+        return render_template('index.html', form=form, name=name)
 
-##response
-#@app.route('/')
+
+#@app.route('/', methods=['GET', 'POST'])
 #def index():
-#    response = make_response('<h1>This document carries a cookie!</h1>')
-#    response.set_cookie('answer', '42')
-#    return response
-
-##redirects to a different url
-#@app.route('/')
-#def index():
-#    return redirect('http://www.example.com')
-
-#aborts if it finds that the user is not a valid user
-
-#@app.route('/user/<id>')
-#def get_user(id):
-#    user = load_user(id)
-#    if not user:
-#        abort(404)
-#        return '<h1>Hello, %s</h1>' % user.name
-
+#    name = None
+#    form = NameForm()
+#    if form.validate_on_submit():
+#        name = form.name.data
+#        form.name.data = ''
+#        return render_template('index.html', form=form, name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
