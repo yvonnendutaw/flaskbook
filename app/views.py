10250c064b38add5fb88f3bb9cbff2f5c147c407
yvonnendutaw <yvonita7@gmail.com>
2016-05-15 21:25:25 +0300

    email confirmation

--- app/views.py
+++ app/views.py
@@ -21,18 +21,42 @@ def account_billing():
     return render_template('account/billing.html')
 
 #forms
-from flask import render_template, redirect, url_for
 
-from . import app
+
+from flask import redirect, render_template, url_for
+
+from . import app, db
 from .forms import EmailPasswordForm
+from .util import ts, send_email
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
+@app.route('/accounts/create', methods=["GET", "POST"])
+def create_account():
     form = EmailPasswordForm()
     if form.validate_on_submit():
+        user = User(
+            email = form.email.data,
+            password = form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
 
-        # Check the password and log the user in
-        # [...]
+        # Now we'll send the email confirmation link
+        subject = "Confirm your email"
 
-        return redirect(url_for('index'))
-    return render_template('login.html', form=form)
+        token = ts.dumps(self.email, salt='email-confirm-key')
+
+        confirm_url = url_for(
+            'confirm_email',
+            token=token,
+            _external=True)
+
+        html = render_template(
+            'email/activate.html',
+            confirm_url=confirm_url)
+
+        # We'll assume that send_email has been defined in myapp/util.py
+        send_email(user.email, subject, html)
+
+        return redirect(url_for("index"))
+
+    return render_template("accounts/create.html", form=form)
