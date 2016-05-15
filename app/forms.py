6ec3088702a02d12586b27bf835398bd682acbca
yvonnendutaw <yvonita7@gmail.com>
2016-05-15 20:26:57 +0300

    caching

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required
-
-class NameForm(Form):
-    user = StringField('What is your name?', validators=[Required()])
-    password = PasswordField('What is your password?' validators=[Required()])
-    submit = SubmitField('Submit')
