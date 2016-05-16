2461a34b6f2c6916b419c586219f1f8536dcf96d
yvonnendutaw <yvonita7@gmail.com>
2016-05-16 21:43:07 +0300

    version 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,4 @@
 from flask import Flask
 
-app = Flask(__name__, instance_relative_config=True)
-
-# Load the default configuration
-app.config.from_object('config.default')
-
-# Load the configuration from the instance folder
-app.config.from_pyfile('config.py')
-
-# Load the file specified by the APP_CONFIG_FILE environment variable
-# Variables defined here will override those in the default configuration
-app.config.from_envvar('APP_CONFIG_FILE')
-app.config.from_object('config')
-
-#calls list converter in util.py
-
-from flask import Flask
-
 app = Flask(__name__)
-
-from .util import ListConverter
-
-app.url_map.converters['list'] = ListConverter
+from app import views
--- app/views.py
+++ app/views.py
@@ -1,62 +1,6 @@
-from flask import render_template
+from app import app
 
-from flask_login import login_required
-
-from . import app
-from .util import check_expired
-
-@app.route('/use_app')
-@login_required
-@check_expired
-def use_app():
-    """Use our amazing app."""
-    # [...]
-    return render_template('use_app.html')
-
-@app.route('/account/billing')
-@login_required
-def account_billing():
-    """Update your billing info."""
-    # [...]
-    return render_template('account/billing.html')
-
-#forms
-
-
-from flask import redirect, render_template, url_for
-
-from . import app, db
-from .forms import EmailPasswordForm
-from .util import ts, send_email
-
-@app.route('/accounts/create', methods=["GET", "POST"])
-def create_account():
-    form = EmailPasswordForm()
-    if form.validate_on_submit():
-        user = User(
-            email = form.email.data,
-            password = form.password.data
-        )
-        db.session.add(user)
-        db.session.commit()
-
-        # Now we'll send the email confirmation link
-        subject = "Confirm your email"
-
-        token = ts.dumps(self.email, salt='email-confirm-key')
-
-        confirm_url = url_for(
-            'confirm_email',
-            token=token,
-            _external=True)
-
-        html = render_template(
-            'email/activate.html',
-            confirm_url=confirm_url)
-
-        # We'll assume that send_email has been defined in myapp/util.py
-        send_email(user.email, subject, html)
-
-        return redirect(url_for("index"))
-
-    return render_template("accounts/create.html", form=form)
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
