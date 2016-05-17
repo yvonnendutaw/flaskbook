d2939cc0a9b9322a349d1890efd4cb60aa1d3755
yvonnendutaw <yvonita7@gmail.com>
2016-05-18 00:54:07 +0300

    redirecting

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from flask import redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'hello world'
+	return redirect('http://google.com')
 
-@app.route('/user/<name>')
-def user(name):
-	return 'hello, %s!' % name
+#@app.route('/user/<name>')
+#def user(name):
+	#return 'hello, %s!' % name
 
 if __name__ == '__main__':
 	app.run(debug=True)
