3a8627e0d7049774a8c364b0a0b81fed693f5d5b
yvonnendutaw <yvonita7@gmail.com>
2016-05-18 00:22:48 +0300

    added a minimal flask application

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,6 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
-from config import basedir
+#server start up
+app = Flask(__name__) 
+#ensures server is started only when the script is executed directly
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
-from app import views, models
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!venv/bin/python
+#server start up
+
 from app import app
 
 app.run(debug=True)
