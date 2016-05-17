cc2e40d9cc5c8d2bbba8710bd354bb234f2aae0b
yvonnendutaw <yvonita7@gmail.com>
2016-05-18 01:50:53 +0300

    used flask-moment

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.script import Manager
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 manager = Manager(app)
 
+moment = Moment(app)
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',
+                            current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
