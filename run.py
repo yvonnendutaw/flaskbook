57401cdf9f016f450eed6237c940e180f8513741
yvonnendutaw <yvonita7@gmail.com>
2016-05-18 01:28:44 +0300

    rendered template

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from flask import Flask
-from flask import redirect
+from flask import render_template
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
-	return redirect('http://google.com')
+	return render_template('index.html')
 
-#@app.route('/user/<name>')
-#def user(name):
-	#return 'hello, %s!' % name
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',
+				name=name)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
