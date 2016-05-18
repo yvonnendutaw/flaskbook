251b9ff3b849285e73b4250f03d6f0bad5cc8c11
yvonnendutaw <yvonita7@gmail.com>
2016-05-18 12:59:51 +0300

    added web forms

--- run.py
+++ run.py
@@ -5,21 +5,31 @@ from flask.ext.script import Manager
 from flask.ext.moment import Moment
 from datetime import datetime
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():
-	return render_template('index.html',
-                            current_time=datetime.utcnow())
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html',
-				name=name)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',
+                               form=form,
+                               name=name)
 
 if __name__ == '__main__':
 	manager.run()
